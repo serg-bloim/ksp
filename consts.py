0136b3eb49a358e8f831726b006251359ba37356
serg.bloim <serg.bloim@gmail.com>
2022-10-11 22:56:34 -0400

    update

--- consts.py
+++ consts.py
@@ -26,5 +26,39 @@ class Body(Enum):
         return 2 * math.pi * sqrt(h ** 3 / self.mu)
 
     def get_circular_alt(self, period):
-        h = ((period/2/math.pi)**2 * self.mu) ** (1./3)
+        h = ((period / 2 / math.pi) ** 2 * self.mu) ** (1. / 3)
+        return h - self.radius
+
+
+class Scanner(Enum):
+    Narrow_band_scanner = (2, 10, 150, 500, False)
+    M700_Survery_scanner = (3, 15, 500, 7500, False)
+    MS_1 = (3, 20, 70, 250, True)
+    MS_2A = (4, 100, 500, 750, True)
+    MS_R = (1.5, 70, 300, 400, True)
+    R_3B_Radar = (1.5, 5, 70, 250, False)
+    R_EO_1_Radar = (3.5, 50, 100, 500, False)
+    SAR_C = (3, 500, 700, 750, False)
+    SAR_L = (4, 250, 500, 1000, False)
+    SAR_X = (1.5, 70, 250, 500, False)
+    SCAN_R = (1, 20, 70, 250, True)
+    SCAN_R2 = (2.5, 70, 250, 500, True)
+    SCAN_RX = (3, 100, 500, 750, True)
+    VS_1 = (1.5, 20, 70, 250, True)
+    VS_11 = (4, 100, 200, 1000, True)
+    VS_3 = (2.5, 70, 350, 500, True)
+
+    def __init__(self, fov, min, best, max, require_daytime) -> None:
+        self.fov = fov
+        self.min = min * 1000
+        self.best = best * 1000
+        self.max = max * 1000
+        self.require_daytime = require_daytime
+
+    def get_period(self, alt):
+        h = alt + self.radius
+        return 2 * math.pi * sqrt(h ** 3 / self.mu)
+
+    def get_circular_alt(self, period):
+        h = ((period / 2 / math.pi) ** 2 * self.mu) ** (1. / 3)
         return h - self.radius
--- scanning_sattelite.py
+++ scanning_sattelite.py
@@ -7,12 +7,12 @@ from operator import itemgetter
 
 import tabulate
 
-from consts import Body
+from consts import Body, Scanner
 
 
 def calc_fov_custom(alt, radius, best_alt, best_fov):
     fov = min(alt, best_alt) / best_alt * best_fov
-    fov = fov * radius / Body.Kerbin.radius
+    fov = fov * Body.Kerbin.radius / radius
     fov = min(fov, 20)
     return fov
 
@@ -29,8 +29,62 @@ def print_period(time_secs):
     hrs, mins = divmod(mins, 60)
     days, hrs = divmod(hrs, 6)
     days_str = "" if days == 0 else f"{int(days)}d "
-    time_str = f"{hrs:02}:{mins:02}:{secs:02}.{ms:.3}"
+    time_str = f"{hrs:02}:{mins:02}:{secs:02}.{ms:.3f}"
     return f"{days_str}{time_str}"
+
+
+def sat_orbits_gen(body, min_alt, max_alt, scan_best_alt, best_fov, angle_overlap=0.0, n_iterations=100):
+    def calc_fov(alt):
+        return calc_fov_custom(alt, body.radius, scan_best_alt, best_fov)
+
+    min_fov = calc_fov(min_alt)
+    max_fov = calc_fov(max_alt)
+    min_period = body.get_period(min_alt)
+    min_period_equator_angle = min_period / body.axisperiod * 360
+    max_k = int(360 // min_period_equator_angle)
+    for k in range(max_k, -1, -1):
+        min_angle = 360 / (k + 1)
+        max_angle = 360 if k == 0 else 360 / k
+        min_period = min_angle / 360 * body.axisperiod
+        alt = body.get_circular_alt(min_period)
+        fov = calc_fov(alt) - angle_overlap
+        min_n = int(ceil(max_angle / fov))
+        for n in range(1, min_n + n_iterations):
+            # test if this n privides full coverage without gaps:
+            t = 1
+            if k == 0 and n == 1:
+                continue
+            angle_resolution = 360 / (k * n + n - t)
+            sat_revolution_angle_shift = angle_resolution * n
+            sat_period = body.axisperiod * sat_revolution_angle_shift / 360
+            sat_alt = body.get_circular_alt(sat_period)
+            fov = calc_fov(sat_alt)
+            if angle_resolution - fov > angle_overlap:
+                continue  # this n value doesn't provide a sufficient gap coverage
+            for t in range(1, n):
+                intersections = [0] * n
+                for x in range(n):
+                    intersect = (x * t) % n
+                    intersections[intersect] += 1
+                if any(x == 0 for x in intersections):
+                    # print(f"RESONANT. k = {k}, n = {n}, t = {t}. Intersections = {intersections}")
+                    break
+                angle_resolution = 360 / (k * n + n - t)
+                # 1 satellite revolution shifts this much of an equatorial angle
+                sat_revolution_angle_shift = angle_resolution * n
+                sat_period = body.axisperiod * sat_revolution_angle_shift / 360
+                sat_alt = body.get_circular_alt(sat_period)
+                if sat_alt < min_alt:
+                    # print(f"Alt is low. k = {k}, n = {n}, t = {t}. alt = {sat_alt} < min_alt ({scan_min_alt})")
+                    continue
+                if sat_alt > max_alt:
+                    # print(f"Alt is too high. k = {k}, n = {n}, t = {t}. alt = {sat_alt} > max_alt ({max_alt})")
+                    break
+                fov = calc_fov(sat_alt)
+                overlap = fov - (sat_revolution_angle_shift / n)
+                total_scan_time = (n * k + n - t) * sat_period
+                # print(f"Alt = {print_alt(sat_alt)} Total scan time = {print_period(total_scan_time)}")
+                yield total_scan_time, k, n, t
 
 
 def calc_precision_factors(precision, n, t):
@@ -43,6 +97,60 @@ def calc_precision_factors(precision, n, t):
         if x == n - 1:
             prv = i
     return -precision / prv, precision / nxt
+
+
+class OrbitDesctiption:
+    angle_resolution: float
+    alt: float
+    max_alt: float
+    min_alt: float
+    max_period: float
+    min_period: float
+    fov: float
+    period: float
+    full_scan_period: float
+    angle_phase: float
+    k: int
+    n: int
+    t: int
+
+
+def describe_orbit(body, k, n, t, scanner):
+    angle_resolution = 360 / (k * n + n - t)
+    sat_revolution_angle_shift = angle_resolution * n
+    sat_period = body.axisperiod * sat_revolution_angle_shift / 360
+    sat_alt = body.get_circular_alt(sat_period)
+    fov = calc_fov_custom(sat_alt, body.radius, scanner.best, scanner.fov)
+    std_angle_precision = fov - angle_resolution
+    std_period_precision = std_angle_precision / sat_revolution_angle_shift * sat_period / (k + 1)
+    angle_precision_neg, angle_precision_pos = calc_precision_factors(std_angle_precision, n, t)
+    period_precision_neg, period_precision_pos = calc_precision_factors(std_period_precision, n, t)
+    sat_min_period = sat_period + period_precision_neg
+    sat_max_period = sat_period + period_precision_pos
+    od = OrbitDesctiption()
+    od.angle_resolution = angle_resolution
+    od.angle_phase = sat_revolution_angle_shift
+    od.period = sat_period
+    od.fov = fov
+    od.min_period = sat_min_period
+    od.max_period = sat_max_period
+    od.min_alt = body.get_circular_alt(sat_min_period)
+    od.max_alt = body.get_circular_alt(sat_max_period)
+    od.alt = body.get_circular_alt(sat_period)
+    od.full_scan_period = (n * k + n - t) * sat_period
+    od.k = k
+    od.n = n
+    od.t = t
+    return od
+
+
+def find_best_orbit(body, scanner: Scanner, min_alt, max_alt, angle_overlap=0.0):
+    orbits = list(sat_orbits_gen(body, min_alt, max_alt, scanner.best, scanner.fov, angle_overlap))
+    orbits.sort(key=itemgetter(0))
+    if orbits:
+        o = orbits[0]
+        total_scan_time, k, n, t = o
+        return describe_orbit(body, k, n, t, scanner)
 
 
 class MyTestCase(unittest.TestCase):
@@ -78,7 +186,8 @@ class MyTestCase(unittest.TestCase):
         sat_alt = body.get_circular_alt(sat_period)
         fov = calc_fov(sat_alt)
 
-        print(f"Alt = {sat_alt}. ({body.get_circular_alt(sat_min_period):.3f} - {body.get_circular_alt(sat_max_period):.3f})")
+        print(
+            f"Alt = {sat_alt}. ({body.get_circular_alt(sat_min_period):.3f} - {body.get_circular_alt(sat_max_period):.3f})")
         print(f"Period = {sat_period}. ({sat_min_period :.3f} - {sat_max_period :.3f})")
         print(f"Fov = {fov}.")
         print(f"Angle precision: {angle_precision_neg} : {angle_precision_pos}")
@@ -127,90 +236,77 @@ class MyTestCase(unittest.TestCase):
         pass
 
     def test_build_min_orbit(self):
-        body = Body.Kerbin
-        min_alt = 70
-        min_alt *= 1000
-        max_alt = 500e3
+        body = Body.Mun
+        scanner = Scanner.VS_3
+        scan_min_alt = scanner.min
+        scan_best_alt = scanner.best
+        scan_max_alt = scanner.max
+        best_fov = scanner.fov
 
-        n_iterations = 100
-
-        scan_min_alt = 70e3
-        scan_best_alt = 250e3
-        scan_max_alt = 500e3
-
-        def calc_fov(alt):
-            return calc_fov_custom(alt, body.radius, scan_best_alt, best_fov)
-
-        best_fov = 4
         angle_overlap = 0.00
-        min_fov = calc_fov(scan_min_alt)
-        max_fov = calc_fov(scan_max_alt)
-        min_period = body.get_period(min_alt)
-        print(f"Alt: {min_alt}, period: {min_period}")
-        min_period_equator_angle = min_period / body.axisperiod * 360
-        max_k = int(360 // min_period_equator_angle)
-        orbits = []
-        for k in range(max_k, -1, -1):
-            min_angle = 360 / (k + 1)
-            max_angle = 360 if k == 0 else 360 / k
-            min_period = min_angle / 360 * body.axisperiod
-            alt = body.get_circular_alt(min_period)
-            fov = calc_fov(alt) - angle_overlap
-            min_n = int(ceil(max_angle / fov))
-            for n in range(1, min_n + n_iterations):
-                # test if this n privides full coverage without gaps:
-                t = 1
-                if k == 0 and n == 1:
-                    continue
-                angle_resolution = 360 / (k * n + n - t)
-                sat_revolution_angle_shift = angle_resolution * n
-                sat_period = body.axisperiod * sat_revolution_angle_shift / 360
-                sat_alt = body.get_circular_alt(sat_period)
-                fov = calc_fov(sat_alt)
-                if angle_resolution - fov > angle_overlap:
-                    continue  # this n value doesn't provide a sufficient gap coverage
-                for t in range(1, n):
-                    intersections = [0] * n
-                    for x in range(n):
-                        intersect = (x * t) % n
-                        intersections[intersect] += 1
-                    if any(x == 0 for x in intersections):
-                        # print(f"RESONANT. k = {k}, n = {n}, t = {t}. Intersections = {intersections}")
-                        break
-                    angle_resolution = 360 / (k * n + n - t)
-                    # 1 satellite revolution shifts this much of an equatorial angle
-                    sat_revolution_angle_shift = angle_resolution * n
-                    sat_period = body.axisperiod * sat_revolution_angle_shift / 360
-                    sat_alt = body.get_circular_alt(sat_period)
-                    if sat_alt < scan_min_alt:
-                        # print(f"Alt is low. k = {k}, n = {n}, t = {t}. alt = {sat_alt} < min_alt ({scan_min_alt})")
-                        continue
-                    if sat_alt > max_alt:
-                        # print(f"Alt is too high. k = {k}, n = {n}, t = {t}. alt = {sat_alt} > max_alt ({max_alt})")
-                        break
-                    fov = calc_fov(sat_alt)
-                    overlap = fov - (sat_revolution_angle_shift / n)
-                    total_scan_time = (n * k + n - t) * sat_period
-                    # print(f"Alt = {print_alt(sat_alt)} Total scan time = {print_period(total_scan_time)}")
-                    orbits.append((total_scan_time, k, n, t))
-            pass
+        min_alt = 100e3
+        min_alt = max(min_alt, scan_min_alt)
+        max_alt = 500e3
+        max_alt = min(max_alt, scan_max_alt)
+        orbits = list(sat_orbits_gen(body, min_alt, max_alt, scan_best_alt, best_fov, angle_overlap))
+        print(f"{scanner.name} @ {body.name}")
+        print(f"FOV     : {best_fov}")
+        print(f"Min alt : {min_alt}")
+        print(f"Best alt: {scan_best_alt}")
+        print(f"Max alt : {scan_max_alt}")
         print(f"Got {len(orbits)} orbits")
         orbits.sort(key=itemgetter(0))
         data = []
         for o in orbits[:100]:
             total_scan_time, k, n, t = o
-            angle_resolution = 360 / (k * n + n - t)
-            sat_revolution_angle_shift = angle_resolution * n
-            sat_period = body.axisperiod * sat_revolution_angle_shift / 360
-            sat_alt = body.get_circular_alt(sat_period)
-            fov = calc_fov(sat_alt)
-            overlap = fov - (sat_revolution_angle_shift / n)
-            period_precision = sat_period * overlap / sat_revolution_angle_shift / n
-            data.append([print_alt(sat_alt), print_period(total_scan_time), print_period(sat_period), fov, overlap,
-                         period_precision, k, n, t])
-        print(tabulate.tabulate(data, headers="Alt,Scan time,Period,fov,a_precision,p_precision,k,n,t".split(",")))
-        # min_n =
-        # for n in range()
+            orb = describe_orbit(body, k, n, t, scanner)
+            data.append(
+                [print_period(total_scan_time),
+                 print_alt(orb.alt),
+                 print_alt(orb.min_alt),
+                 print_alt(orb.max_alt),
+                 print_period(orb.period),
+                 orb.fov,
+                 print_period(orb.min_period),
+                 print_period(orb.max_period),
+                 k, n, t])
+        print(tabulate.tabulate(data,
+                                headers="Scan time,Alt,Min alt,max alt,Period,fov,min_period,max_period,k,n,t".split(
+                                    ",")))
+
+    def test_miltiple_scanners(self):
+        body = Body.Minmus
+        scanners = [Scanner.VS_11, Scanner.R_3B_Radar, Scanner.R_EO_1_Radar, Scanner.SAR_X,
+                    Scanner.SCAN_R2]
+        min_alt = 140.5e3
+        max_alt = 150e3
+        print(f"Scanners for {body.name}")
+        data = []
+        for s in scanners:
+            orb = find_best_orbit(body, s, max(min_alt, s.min), min(max_alt, s.max))
+            if orb:
+                data.append(
+                    [s.name, print_period(orb.full_scan_period), print_alt(orb.alt), orb.k, orb.n, orb.t, orb.fov,
+                     print_alt(orb.min_alt), print_alt(orb.max_alt)])
+            else:
+                data.append([s.name, "---"])
+        print(tabulate.tabulate(data, headers="Name,period,Alt,K,N,T,FOV,Min alt,Max alt".split(",")))
+
+    def test_simulate_miltiple_scanners(self):
+        body = Body.Minmus
+        k, n, t = 2, 15, 1
+        scanners = [Scanner.VS_11, Scanner.R_3B_Radar, Scanner.R_EO_1_Radar, Scanner.SAR_X, Scanner.SAR_L,
+                    Scanner.SCAN_R, Scanner.SCAN_R2, Scanner.SCAN_RX,Scanner.Narrow_band_scanner]
+        orb = describe_orbit(body, k, n, t, Scanner.SAR_X)
+        min_alt = orb.alt
+        max_alt = orb.alt
+        print(f"Scanners for {body.name}")
+        data = []
+        for s in scanners:
+            ord = find_best_orbit(body, s, max(min_alt, s.min), min(max_alt, s.max))
+            if ord:
+                data.append([s.name, print_period(ord.full_scan_period), print_alt(ord.alt), ord.k, ord.n, ord.t])
+        print(tabulate.tabulate(data, headers="Name,period,Alt,K,N,T".split(",")))
 
 
 if __name__ == '__main__':
